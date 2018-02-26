64a05bd69b25e100e90af1058910f1cecbaa83b7
jkjawbreaker <jkjawbreaker@gmail.com>
2018-02-26 15:10:36 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,27 @@ package com.company;
 // ---------*---------*---------*---------*---------*
 // The use of static imports is something that should be used carefully.
 // I have only ever used this technique for the project wide constants.
-//
-import javax.swing.*;
 
+import javax.swing.*;
+import java.util.*;
 import static com.company.ProjConstants.*;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
-        // ---------*---------*---------*---------*---------*
-        // Create a new object from the StDeviation Class
-        StDeviation calcSDev =  new StDeviation();
-        JFrame Ask=new JFrame("Standard Deviation Program");
-        JFrame Answer=new JFrame("Standard Deviation Program");
-        JTextField textField=new JTextField("");
-        JPanel panel= new JPanel();
-        Ask.setVisible(true);
-        Ask.setSize(300,300);
-        panel.setSize(100,100);
-        Ask.setLocationRelativeTo(null);
-        Ask.add(panel);
-        textField.setSize(100,100);
-        panel.add(textField);
+        boolean repeat = true;
+        int[] numbers = new int[0];
+        String fileName;
+        System.out.println("What would you like to name the file your numbers are stored in");
+        Scanner s1 = new Scanner(System.in), s2 = new Scanner(System.in);
+        do {
+            switch (s2.nextInt()) {
+                case (): {
+
+                }
+                default:{
+                    System.out.println("You need to enter positive number");
+                }
+            }
+        }while (repeat) ;
     }
 }
