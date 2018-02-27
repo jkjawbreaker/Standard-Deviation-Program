361416b64cf55afe70f9e846baa36c2e432e6f2b
jkjawbreaker <jkjawbreaker@gmail.com>
2018-02-27 14:57:08 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,19 +11,27 @@ import static com.company.ProjConstants.*;
 public class Main {
     public static void main(String[] args) {
         boolean repeat = true;
-        int[] numbers = new int[0];
+        int enterInt,counter=0,maxarray=1;
+        int numbers[]=new int[maxarray];
         String fileName;
         System.out.println("What would you like to name the file your numbers are stored in");
         Scanner s1 = new Scanner(System.in), s2 = new Scanner(System.in);
         do {
-            switch (s2.nextInt()) {
-                case (): {
-
-                }
-                default:{
-                    System.out.println("You need to enter positive number");
+            s1 = new Scanner(System.in);
+            if(s1.hasNextInt()){
+                enterInt=s1.nextInt();
+                maxarray=maxarray+1;
+                numbers[counter]=enterInt;
+                System.out.println(enterInt);
+            }else if (s1.nextLine()=="done"){
+                for(int a=0;a<counter;a++){
+                    System.out.print(numbers);
                 }
             }
+            else {
+                System.out.println(counter);
+            }
+            counter=counter+1;
         }while (repeat) ;
     }
 }
